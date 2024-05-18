fb729e538ef3c7aaa60a65b8bd3e6b4aa9cc3f13
mtdiedrich <mitchdiedrich@gmail.com>
2024-05-18 15:50:01 -0500

    Too many changes.

--- click_pro/website_hosting_stack.py
+++ click_pro/website_hosting_stack.py
@@ -1,9 +1,10 @@
 from aws_cdk import (
     aws_s3 as s3,
+    aws_s3_deployment as s3deploy,
     Stack,
-    RemovalPolicy
 )
 from constructs import Construct
+import aws_cdk as cdk
 
 class WebsiteHostingStack(Stack):
 
@@ -13,8 +14,20 @@ class WebsiteHostingStack(Stack):
         # Create S3 bucket for hosting the website
         website_bucket = s3.Bucket(self, "WebsiteBucket",
             website_index_document="index.html",
+            website_error_document="error.html",
             public_read_access=True,
-            removal_policy=RemovalPolicy.DESTROY
+            removal_policy=cdk.RemovalPolicy.DESTROY
+        )
+
+        # Deploy website contents to S3 bucket
+        s3deploy.BucketDeployment(self, "DeployWebsite",
+            sources=[s3deploy.Source.asset("./website")],
+            destination_bucket=website_bucket,
+        )
+
+        # Output the website URL
+        cdk.CfnOutput(self, "WebsiteURL",
+            value=website_bucket.bucket_website_url
         )
 
         self.website_bucket = website_bucket
