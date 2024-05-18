4c6b2ae5dd6889117450d8bdd55317c0d63965db
mtdiedrich <mitchdiedrich@gmail.com>
2024-05-18 14:21:15 -0500

    Pythonify lambda package (#6)
    Pythonify lambda package

--- click_pro/click_pro_stack.py
+++ click_pro/click_pro_stack.py
@@ -2,37 +2,41 @@ from aws_cdk import (
     aws_lambda as lambda_,
     aws_apigateway as apigateway,
     aws_s3 as s3,
-    aws_s3_assets as s3_assets,
-    Stack
+    Stack,
 )
-
-import os
-
+from aws_cdk.aws_lambda_python_alpha import PythonLayerVersion
 from constructs import Construct
+import os
 
 class ClickProStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        # Read video ID from environment variable
+        video_id = os.getenv('VIDEO_ID', 'default_video_id')
+
         # Create S3 bucket for credentials
         bucket = s3.Bucket(self, "ephemeral-z90d7fs9dfwq")
 
-        # Upload the deployment package to S3
-        s3_deployment = s3_assets.Asset(self, "LambdaPackage",
-            path="lambda/lambda_package.zip"
+        # Define Lambda layer for dependencies
+        dependencies_layer = PythonLayerVersion(
+            self, "DependenciesLayer",
+            entry="lambda/dependencies",
+            compatible_runtimes=[lambda_.Runtime.PYTHON_3_9]
         )
 
         # Define Lambda function
         thumbnail_switcher_lambda = lambda_.Function(
             self, "ThumbnailSwitcherFunction",
             runtime=lambda_.Runtime.PYTHON_3_9,
-            handler="handler.lambda_handler",
-            code=lambda_.Code.from_bucket(s3_deployment.bucket, s3_deployment.s3_object_key),
+            handler="index.lambda_handler",
+            code=lambda_.Code.from_asset("lambda/function"),
+            layers=[dependencies_layer],
             environment={
                 "S3_BUCKET_NAME": bucket.bucket_name,
                 "S3_OBJECT_KEY": "cred.json",
-                "VIDEO_ID": os.getenv('VIDEO_ID', 'default_video_id')  # Use the passed environment variable or a default
+                "VIDEO_ID": video_id
             }
         )
 
