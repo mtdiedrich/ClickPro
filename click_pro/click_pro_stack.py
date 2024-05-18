2ce4bb88483eafc49ae05d86f9b4e1d48b944c78
mtdiedrich <mitchdiedrich@gmail.com>
2024-05-18 10:50:11 -0500

    Extend functionality.

--- click_pro/click_pro_stack.py
+++ click_pro/click_pro_stack.py
@@ -1,28 +1,34 @@
 from aws_cdk import (
-    cdk,
+    core,
     aws_lambda as lambda_,
     aws_apigateway as apigateway,
     aws_s3 as s3,
+    aws_s3_assets as s3_assets
 )
 
-class ClickProStack(cdk.Stack):
+class ClickProStack(core.Stack):
 
-    def __init__(self, scope: cdk.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # Create S3 bucket for credentials
-        bucket = s3.Bucket(self, "YouTubeCredentialsBucket")
+        bucket = s3.Bucket(self, "ephemeral-z90d7fs9dfwq")
+
+        # Upload the deployment package to S3
+        s3_deployment = s3_assets.Asset(self, "LambdaPackage",
+            path="lambda/lambda_package.zip"
+        )
 
         # Define Lambda function
         thumbnail_switcher_lambda = lambda_.Function(
             self, "ThumbnailSwitcherFunction",
             runtime=lambda_.Runtime.PYTHON_3_9,
             handler="handler.lambda_handler",
-            code=lambda_.Code.from_asset("lambda"),
+            code=lambda_.Code.from_bucket(s3_deployment.bucket, s3_deployment.s3_object_key),
             environment={
                 "S3_BUCKET_NAME": bucket.bucket_name,
-                "S3_OBJECT_KEY": "path/to/credentials.json",
-                "VIDEO_ID": "your_video_id"
+                "S3_OBJECT_KEY": "cred.json",
+                "VIDEO_ID": "ZhIUkujgPu0"
             }
         )
 
@@ -38,4 +44,3 @@ class ClickProStack(cdk.Stack):
         thumbnail_resource = api.root.add_resource("thumbnail")
         thumbnail_resource.add_method("POST", apigateway.LambdaIntegration(thumbnail_switcher_lambda),
                                       api_key_required=False)
-
