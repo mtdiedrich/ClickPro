485f53749b1eb7e377ee96709759f0fad868a6c7
mtdiedrich <mitchdiedrich@gmail.com>
2024-05-18 13:04:30 -0500

    Update to pythonify lambda.

--- click_pro/click_pro_stack.py
+++ click_pro/click_pro_stack.py
@@ -2,37 +2,37 @@ from aws_cdk import (
     aws_lambda as lambda_,
     aws_apigateway as apigateway,
     aws_s3 as s3,
-    aws_s3_assets as s3_assets,
-    Stack
+    aws_lambda_python_alpha as lambda_python,
+    Stack,
 )
-
-import os
-
 from constructs import Construct
+import os
 
 class ClickProStack(Stack):
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        # Read video ID from environment variable
+        video_id = os.getenv('VIDEO_ID', 'default_video_id')
+
         # Create S3 bucket for credentials
         bucket = s3.Bucket(self, "ephemeral-z90d7fs9dfwq")
 
-        # Upload the deployment package to S3
-        s3_deployment = s3_assets.Asset(self, "LambdaPackage",
-            path="lambda/lambda_package.zip"
-        )
-
-        # Define Lambda function
-        thumbnail_switcher_lambda = lambda_.Function(
+        # Define Lambda function with bundling
+        thumbnail_switcher_lambda = lambda_python.PythonFunction(
             self, "ThumbnailSwitcherFunction",
+            entry="lambda",  # The directory where your handler.py and requirements.txt are
             runtime=lambda_.Runtime.PYTHON_3_9,
-            handler="handler.lambda_handler",
-            code=lambda_.Code.from_bucket(s3_deployment.bucket, s3_deployment.s3_object_key),
+            index="handler.py",  # The file containing the handler function
+            handler="lambda_handler",
+            bundling={
+                "pip": True,  # Use Docker to install dependencies with pip
+            },
             environment={
                 "S3_BUCKET_NAME": bucket.bucket_name,
                 "S3_OBJECT_KEY": "cred.json",
-                "VIDEO_ID": os.getenv('VIDEO_ID', 'default_video_id')  # Use the passed environment variable or a default
+                "VIDEO_ID": video_id
             }
         )
 
